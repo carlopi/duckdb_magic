a7591909a5bc84cd7f15ddc428d9d808a38ba8d4
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-03-20 15:38:42 +0100

    Adapt to compile to wasm targets

--- extension_config.cmake
+++ extension_config.cmake
@@ -4,6 +4,7 @@
 duckdb_extension_load(magic
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
+    LINKED_LIBS "../../vcpkg_installed/wasm32-emscripten/lib/lib*.a"
 )
 
 # Any extra extensions that should be built
